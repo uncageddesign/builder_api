937fc65921ee66bc1194e095441d81dba64b6475
mhairimccrindle@gmail.com <mhairimccrindle@gmail.com>
2020-04-13 00:46:30 +0100

    building seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -1,9 +1,46 @@
 use character_builder;
 db.dropDatabase();
 
-db.builder.insertMany([
+db.classes.insertMany([
   {
-    name: "Barbarian"
+    name: "Barbarian",
+    equipment: {
+      weaponOne: [
+        "Greataxe 1D12 Slashing",
+        "Battleaxe 1D8 Slashing",
+        "Glaive 1D10 Slashing",
+        "Warhammer 1D8 Bludgeoning"
+      ],
+      weaponTwo: [
+        "Handaxes(2) 1D6 Slashing",
+        "Dagger 1D4 Piercing"
+      ],
+      pack: ["Explorer's Pack"],
+      extraItem: "Javelins(4) 1D6 Slashing"
+    },
+    armour: [
+      "Light Armour",
+      "Medium Armour",
+      "Shield"
+    ],
+    savingThrows: [
+      "Strength",
+      "Constitution"
+    ],
+    skills: [
+      "Animal Handling",
+      "Athletics",
+      "Intimidation",
+      "Nature",
+      "Perception",
+      "Survival"
+    ],
+    hitDie: "D12",
+    proficiency: "+2",
+    features: {
+      rages: 2,
+      rageDamage: 2
+    }
   },
   {
     name: "Cleric"
@@ -13,5 +50,20 @@ db.builder.insertMany([
   },
   {
     name: "Fighter"
+  }
+]);
+
+db.races.insertMany([
+  {
+    name: "Human"
+  },
+  {
+    name: "Orc"
+  },
+  {
+    name: "Elf"
+  },
+  {
+    name: "Dwarf"
   }
 ]);
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const parser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router');
 const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 app.use(parser.json());
 
 MongoClient.connect('mongodb://localhost:27017', (err, client) => {
@@ -14,9 +15,13 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
   }
 
   const db = client.db('character_builder');
-  const builderCollection = db.collection('builder');
-  const builderRouter = createRouter(builderCollection)
-  app.use('/api/builder', builderRouter);
+  const classesCollection = db.collection('classes');
+  const classesRouter = createRouter(classesCollection)
+  app.use('/api/classes', classesRouter);
+
+  const racesCollection = db.collection('races');
+  const racesRouter = createRouter(racesCollection)
+  app.use('/api/races', racesRouter);
 
   app.listen(3000, function(){
     console.log(`app listening on port ${this.address().port}`);
